f7b7629bd4d26c4f2d40ea4076fe075afa673209
Alyssa Larios <alyssalarios@g.harvard.edu>
2020-10-05 20:59:12 -0400

    permute data so first dimmension is time. take cells as separate variables in workspace and save mat file

--- preprocessVisualStimMovie.m
+++ preprocessVisualStimMovie.m
@@ -10,13 +10,24 @@
 % Next we parse the concatenated movie to split it into stim presentation 
 % type and trials. This is a cell array with each block containing a movie
 % of that particular trial. Each row is a stim position/type and each
-% column is an individual trial. this is parsedData variable.
+% column is an individual trial. this is parsedData variable. 
+
+% Movies for each trial are then averaged together to get a stimulation
+% position average. Df is computes by subtracting the average of normFrames
+% from each frame in the movie. 
+
+% data is then separated into individual variables - change number of
+% variables manually to accomodate changing stimnulus conditions
 
 %% Routine parameters
 concatRawSave = 0; %starting from raw/ split up data movies or not
 makeGridTifSave = 0;%save grid tif or not
     % if saving, go into Gridtif chunk and make sure dimmensions (gridDims)
     %are correct for stimulus parameters
+saveData = 1; % if 1, saves preprocessed data in a mat file. 
+    % 
+
+
 normFrames = 8:14;
 stimFrames = 25:35;
 stimType = 'bpNoise'; %takes 'bpNoise' or 'driftGrating'
@@ -82,5 +93,41 @@ switch stimType
 
         % average all trials and subtract baseline
         DfAllTrials = avgDf(parsedData,normFrames);
+    case 'driftGrating'
+        %load movie and grating type
+        catMovieDir = dir('*cat.tif'); 
+        metaFile = dir('*.mat');
+        
+        catMovie = loadtiff(catMovieDir.name);
+        load(metaFile.name,'rectPositions'); %change this to luke variable
+        
+        parsedData = parseVisualStimData(catMovie,rectPositions); % depending on how we 
+        %change the bpNoise stim presentation, might need to go back into
+        %this function and make it apply properly
+%       Parameters for this function - name/value pairs
+%           data (required raw tif)
+%           rectPositions (required 1x2xn dimmensional array specifying stim
+%               locations)
+%           numPosns = number of locations possible (default 9)
+%           numTrials = number of trials per location (default 6)
+%           numFramesperTrial = number of frames in movie per trial (default 55)
+%           posVector = locations on screen that stims can take default [.2,.5,.8]
+
+        % average all trials and subtract baseline
+        DfAllTrials = avgDf(parsedData,normFrames);
 end
 
+%% Separate averaged movies into new variables and save
+
+% permute so that first dimmension is time 
+permutedAllTrials = cellfun(@(x) permute(x,[3,1,2]),DfAllTrials,...
+    'un',false);
+
+topDown = permutedAllTrials{1,1};
+bottomUp = permutedAllTrials{2,1};
+leftRight = permutedAllTrials{3,1};
+rightLeft = permutedAllTrials{4,1};
+
+if saveData
+    save([dataPath,'/DfMovies.mat'],'topDown','bottomUp','leftRight','rightLeft');
+end
